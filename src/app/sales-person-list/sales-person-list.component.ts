7ce1d2347555c1ce5d4c3f4393dab4df2e7047dd
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-10-16 12:53:00 +0300

    Sales person component and integrating data.

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from '../sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.less']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] =[
+    new SalesPerson("Murat","Yilmaz","mrt.yilmaz743@gmail.com",1000),
+    new SalesPerson("Mert","Yildiz","mrt.yildiz@gmail.com",3000),
+    new SalesPerson("Omer","Yilmaz","Omr.yilmaz744@gmail.com",5000)
+  ]
 
   constructor() { }
 
---
+++ src/app/sales-person.ts
@@ -0,0 +1,8 @@
+export class SalesPerson {
+    constructor(public firstName: string,
+                public lastname: string,
+                public eMail: string,
+                public saleVolume: number){
+        
+    }
+}
